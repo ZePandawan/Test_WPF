e1a4b69d1c1a4fb8cc655ea57874891acd2a94c4
Vincent_Reix_54 <vincentreix54@gmail.com>
2022-02-16 16:16:18 +0100

    Many changes

--- Test_WPF/MainWindow.xaml.cs
+++ Test_WPF/MainWindow.xaml.cs
@@ -29,24 +29,13 @@ namespace Test_WPF
         {
             var openFileDialog1 = new Microsoft.Win32.OpenFileDialog();
             openFileDialog1.Filter = "Images (.jpeg)(.jpg)|*.jpg|Images (.png)|*.png";
-            openFileDialog1.Title = "Ouvrir une nouvelle photo";
+            openFileDialog1.Title = "Choix photo";
             openFileDialog1.DefaultExt = ".jpg";
 
             if (openFileDialog1.ShowDialog() == true){
                 var test = openFileDialog1.FileName;
                 TestLabel.Content = test;
             }
-            
-            /*
-            if(testImage.Source != null)
-            {
-                testImage.Source = null;
-            }
-            else
-            {
-                testImage.Source = "/Capture.jpg";
-            }*/
-
         }
     }
 }
